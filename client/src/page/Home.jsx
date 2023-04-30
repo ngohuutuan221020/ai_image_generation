eb9b973233f97c26e89b2b8c4d6f1ec1117cce61
Professor <ngohuutuan221020@gmail.com>
2023-04-30 13:31:37 +0700

    env

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://ai-image-generation-vvdr.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
